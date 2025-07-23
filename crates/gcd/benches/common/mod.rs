569be36dd9e3f64972cb5f33d0ddfb027e6e5c6d
YEUNG King On <koyeung@gmail.com>
2025-07-23 12:07:58 +0800

    fix: fix after cargo update

--- crates/gcd/benches/common/mod.rs
+++ crates/gcd/benches/common/mod.rs
@@ -1,4 +1,4 @@
-use rand::distributions::Standard;
+use rand::distr::StandardUniform;
 use rand::{Rng, SeedableRng};
 
 pub(crate) fn prepare() -> (Box<[u64]>, Box<[u64]>) {
@@ -6,11 +6,11 @@ pub(crate) fn prepare() -> (Box<[u64]>, Box<[u64]>) {
 
     let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(0xbeef_cafe);
     let xs = (&mut rng)
-        .sample_iter(Standard)
+        .sample_iter(StandardUniform)
         .take(SAMPLES)
         .collect::<Box<_>>();
     let ys = (&mut rng)
-        .sample_iter(Standard)
+        .sample_iter(StandardUniform)
         .take(SAMPLES)
         .collect::<Box<_>>();
 
--- crates/sum-float/benches/common/mod.rs
+++ crates/sum-float/benches/common/mod.rs
@@ -1,6 +1,6 @@
-use rand::{distributions::Standard, Rng, SeedableRng};
+use rand::{distr::StandardUniform, Rng, SeedableRng};
 
 pub(crate) fn prepare(len: usize) -> Vec<f32> {
     let rng = rand_chacha::ChaCha8Rng::seed_from_u64(0xbeef_cafe);
-    rng.sample_iter(Standard).take(len).collect()
+    rng.sample_iter(StandardUniform).take(len).collect()
 }
