e7a3c4818188f7c87be95fdf23499aa60403a952
YEUNG King On <koyeung@gmail.com>
2026-02-03 01:54:25 +0000

    fix: remove rand_chacha

--- crates/gcd/benches/common/mod.rs
+++ crates/gcd/benches/common/mod.rs
@@ -1,10 +1,11 @@
 use rand::distr::StandardUniform;
-use rand::{Rng, SeedableRng};
+use rand::prelude::{Rng, SeedableRng, StdRng};
 
 pub(crate) fn prepare() -> (Box<[u64]>, Box<[u64]>) {
     const SAMPLES: usize = 128;
 
-    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(0xbeef_cafe);
+    let mut rng = StdRng::seed_from_u64(0xbeef_cafe);
+
     let xs = (&mut rng)
         .sample_iter(StandardUniform)
         .take(SAMPLES)
--- crates/sum-float/benches/common/mod.rs
+++ crates/sum-float/benches/common/mod.rs
@@ -1,6 +1,7 @@
-use rand::{Rng, SeedableRng, distr::StandardUniform};
+use rand::distr::StandardUniform;
+use rand::prelude::{Rng, SeedableRng, StdRng};
 
 pub(crate) fn prepare(len: usize) -> Vec<f32> {
-    let rng = rand_chacha::ChaCha8Rng::seed_from_u64(0xbeef_cafe);
+    let rng = StdRng::seed_from_u64(0xbeef_cafe);
     rng.sample_iter(StandardUniform).take(len).collect()
 }
